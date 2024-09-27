04da172a2b5c25657467b75f052093231678c1b9
STSumu <sabihatahsinsumu@gmail.com>
2024-09-27 20:21:49 +0600

    modified header and pageswitch.js

--- js/pageSwitch.js
+++ js/pageSwitch.js
@@ -2,7 +2,7 @@
 // pageswitch between home and blog 
 document.getElementById('blog-btn').addEventListener('click',function(){
     window.location.href="blog.html";
-})
+});
 document.getElementById('home-btn').addEventListener('click',function(){
     window.location.href="index.html";
-})
+});
